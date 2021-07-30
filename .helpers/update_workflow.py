306f5753c57f2a8e07d9abe9ef7f76aa9ac09ff5
actualben <actualben@users.noreply.github.com>
2021-07-30 03:06:33 +0200

    updating keypath after workflow change

--- .helpers/update_workflow.py
+++ .helpers/update_workflow.py
@@ -7,7 +7,7 @@ from typing import Any, Dict, Final, List
 
 from ruamel.yaml import YAML
 
-CONTAINER_KEYPATH: Final = "jobs.build.strategy.matrix.container"
+CONTAINER_KEYPATH: Final = "jobs.build-push.strategy.matrix.container"
 
 
 def build_contexts(base_str: str) -> List[str]:
