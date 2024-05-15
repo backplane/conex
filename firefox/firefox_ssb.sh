5d69af4a4a2cf6d93d79e420a1ac76a857797c00
actualben <actualben@users.noreply.github.com>
2024-05-15 10:46:11 +0200

    adjust RUN style; update debian deps; disable seccomp profile for now

--- firefox/firefox_ssb.sh
+++ firefox/firefox_ssb.sh
@@ -34,20 +34,30 @@ firefox_ssb() {
     fi
   fi
 
-  SECCOMP_PROFILE="${SSB_BASE}/seccomp.json"
-  if ! [ -f "$SECCOMP_PROFILE" ]; then
-    if ! curl -sSLf --tlsv1.2 -o "$SECCOMP_PROFILE" \
-      "https://raw.githubusercontent.com/backplane/conex/master/firefox/seccomp.json"; then
-      echo "unable to obtain seccomp profile from github" 2>&1
-      return 1
-    fi
-  fi
+  # 15-May-2024 disabled for now
+  #SECCOMP_PROFILE="${SSB_BASE}/seccomp.json"
+  #if ! [ -f "$SECCOMP_PROFILE" ]; then
+  #  if ! curl -sSLf --tlsv1.2 -o "$SECCOMP_PROFILE" \
+  #    "https://raw.githubusercontent.com/backplane/conex/master/firefox/seccomp.json"; then
+  #    echo "unable to obtain seccomp profile from github" 2>&1
+  #    return 1
+  #  fi
+  #fi
+  #
+  #  --security-opt "seccomp=${SECCOMP_PROFILE}"
 
   # not on macOS:
   #  --device /dev/snd
   #  --device /dev/dri
   #  --volume "/tmp/.X11-unix:/tmp/.X11-unix"
   #  --volume /dev/shm:/dev/shm
+
+  # if DISPLAY is blank or a file path, set it to docker container's host address
+  case "${DISPLAY:-}" in
+    ""|/*)
+      DISPLAY="host.docker.internal:0"
+      ;;
+  esac
 
   docker run \
     $SSB_DETACH \
@@ -56,12 +66,11 @@ firefox_ssb() {
     --tty \
     --cpuset-cpus "${SSB_CPUS:-0}" \
     --memory "${SSB_MEM:-512mb}" \
-    --env "DISPLAY=${DISPLAY:-host.docker.internal:0}" \
+    --env "DISPLAY" \
     --volume "firefox_ssb_${SITE}:/data" \
     --volume "${HOME}/Downloads:/home/user/Downloads" \
-    --security-opt "seccomp=${SECCOMP_PROFILE}" \
     --name "firefox_ssb_${SITE}" \
-    "backplane/firefox" \
+    "${FIREFOX_SSB_IMAGE:-backplane/firefox}" \
     "$@"
   [ -n "$SSB_DEBUG" ] && printf '%s\n' \
     "" \
